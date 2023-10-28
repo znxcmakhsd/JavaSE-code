5f2e7af8ff84391cc1015e388d9a665b2ae32af5
qyx101 <xmqyx1129@gmail.com>
2023-10-28 21:07:45 +0800

    10-28

---
+++ 10-28/Test.java
@@ -0,0 +1,20 @@
+public class TestDemo {
+    public static void main(String[] args) {
+        //byte b1 = 200;
+        //char c2 = '中国';
+       /* float f = 54.103f;
+        System.out.println(f);*/
+        //boolean b1 = true;
+
+   /*     double deci = 3.14;
+        float d2 = 2.769343f;
+        int i1 = 365;
+        int i2 = 12;
+        char ch = '吃';
+        boolean bool = false;
+        String str = "不可描述";
+        System.out.println(d2);*/
+
+
+    }
+}
