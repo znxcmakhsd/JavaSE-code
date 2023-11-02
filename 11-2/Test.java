ce125676b1029d54dbedf9833a91c119658326a0
qyx101 <xmqyx1129@gmail.com>
2023-11-02 16:43:47 +0800

    11-2

--- 11-2/Test.java
+++ 11-2/Test.java
@@ -2,10 +2,81 @@ import java.lang.reflect.Array;
 import java.util.Arrays;
 
 public class TestDemo {
-    public static void main5(String[] args) {
-        // 9.
+    public static void main10(String[] args) {
+        int[] nums = {11,2,15,7};
+        int target = 9;
+        int[] ret = test(nums,target);
+        System.out.println(Arrays.toString(ret));
     }
-    public static void main(String[] args) {
+    public static int[] test(int[] nums, int target) {
+        int[] ret = new int[2];
+        for (int i = 0; i < nums.length-1; i++) {
+            for (int j = i; j < nums.length-i-1; j++) {
+                if (nums[i] + nums[j+1] == target) {
+                    int sum = nums[i] + nums[j+1];
+                    ret[0] = i;
+                    ret[1] = j+1;
+                    break;
+                }
+            }
+        }
+        return ret;
+    }
+    public static void main9(String[] args) {
+        /*
+        调整数组顺序使得奇数位于偶数之前。调整之后，不关心大小顺序。
+        如数组：[1,2,3,4,5,6]
+        调整后可能是：[1, 5, 3, 4, 2, 6]
+        */
+        int[] arr = {1,2,3,4,5,6};
+        int[] newArr = new int[arr.length];
+        int index = 0;
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] % 2 != 0) {
+                newArr[index] = arr[i];
+                index++;
+            }
+        }
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] % 2 == 0) {
+                newArr[index] = arr[i];
+                index++;
+            }
+        }
+        System.out.println(Arrays.toString(newArr));
+
+    }
+    public static void main8(String[] args) {
+        // 实现一个方法 sum, 以数组为参数, 求数组所有元素之和.
+        int[] arr = {1,2,3,4,5};
+        System.out.println(sum(arr));
+    }
+    public static int sum(int[] arr) {
+        int ret = 0;
+        for (int i = 0; i < arr.length; i++) {
+            ret += arr[i];
+        }
+        return ret;
+    }
+    public static void main7(String[] args) {
+        int[] arr = {1,2,3};
+        arr = transform(arr);
+        System.out.println(Arrays.toString(arr));
+    }
+    public static int[] transform(int[] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] *= 2;
+        }
+        return arr;
+    }
+    public static void main6(String[] args) {
+        int[] arr = new int[100];
+        for (int i = 0; i < 100; i++) {
+            arr[i] = i + 1;
+        }
+        System.out.println(Arrays.toString(arr));
+    }
+    public static void main5(String[] args) {
         // 8. 数组拷贝 + 扩容 + 指定位置拷贝
         int[] arr = {1,2,3};
         //int[] copyArr = Arrays.copyOf(arr,arr.length*2);
