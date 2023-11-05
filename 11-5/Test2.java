06c4924a204e9e44eb01183e9844f1f6dbbd42a5
qyx101 <xmqyx1129@gmail.com>
2023-11-05 20:48:55 +0800

    11-5

---
+++ 11-5/Test2.java
@@ -0,0 +1,56 @@
+import java.util.Arrays;
+
+/*public class TestDemo {
+    public static int[] sum(int[] nums,int target) {
+        int ret[] = new int[2];
+        for (int i = 0; i < nums.length-1; i++) {
+            for (int j = nums.length-1; j > i; j--) {
+                if (nums[i] + nums[j] == target) {
+                    ret[0] = i;
+                    ret[1] = j;
+                }
+            }
+        }
+        return ret;
+    }
+    public static int funcXor(int[] nums) {
+        int ret = 0;
+        for (int i = 0; i < nums.length; i++) {
+            ret ^= nums[i];
+        }
+        return ret;
+    }
+    public static void main(String[] args) {
+       *//*int[] nums = {2,7,11,15};
+       int target = 9;
+       System.out.println(Arrays.toString(sum(nums, target)));*//*
+       *//* int[] nums = {4,1,2,1,2};
+        System.out.println(funcXor(nums));*//*
+
+        String str;
+        System.out.println("s="+str);
+    }
+}*/
+
+/*
+public class TestDemo{
+
+    private int count;
+
+    public static void main(String[] args) {
+
+        TestDemo test=new TestDemo(88);
+
+        System.out.println(test.count);
+
+    }
+
+    TestDemo(int a) {
+
+        count=a;
+
+    }
+
+}
+*/
+
