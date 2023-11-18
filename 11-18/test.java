798fb09bca53f5e2ecfa04bdb9cee95f33e8dc67
qyx101 <xmqyx1129@gmail.com>
2023-11-18 10:11:57 +0800

    11-18 school thing.....

---
+++ 11-18/test.java
@@ -0,0 +1,91 @@
+import java.util.Scanner;
+
+/*
+public class Main{
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        double n1 = scanner.nextDouble();
+        double n2 = scanner.nextDouble();
+        double n3 = scanner.nextDouble();
+        System.out.printf("%.2f",n1+n2+n3);
+    }
+}*/
+
+
+/*
+public class Main{
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int x = scanner.nextInt();
+        double y = (2.0*x+1)/(7*x+9)+12*x;
+        System.out.printf("%.2f",y);
+    }
+}*/
+
+/*
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int m = scanner.nextInt();
+        int n = scanner.nextInt();
+        if (m%n == 0) {
+            System.out.println("Divisible");
+        } else {
+            System.out.println("Not divisible");
+        }
+    }
+}
+*/
+
+/*public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int input = scanner.nextInt();
+        if (input >= 75) {
+            System.out.println("A");
+        } else if (input >= 60 && input <= 74) {
+            System.out.println("B");
+        }else if (input >= 45 && input <= 59) {
+            System.out.println("C");
+        }else if (input >= 35 && input <= 44) {
+            System.out.println("D");
+        } else if (input <= 35) {
+            System.out.println("E");
+        }else {
+            System.out.println("Invalid");
+        }
+    }
+}*/
+
+
+import java.util.Scanner;
+
+/*public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        double sum = 0;
+        double i = scanner.nextDouble();
+        for (; i != 0; i = scanner.nextDouble()) {
+            sum += i;
+        }
+        System.out.printf("%.2f", sum);
+    }
+}*/
+
+/*
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        for (int i = 0;i < n;i++) {
+            double sum = 0;
+            for (double j = scanner.nextDouble(); j != 0; j = scanner.nextDouble()) {
+                sum += j;
+            }
+            System.out.printf("%.2f",sum);
+            System.out.println();
+        }
+    }
+}
+
+*/
